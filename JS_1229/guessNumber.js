6600c54115b7ffc16a44c05548ca9dad8adf0a6b
tp12356twNccu <146693497+tp12356twNccu@users.noreply.github.com>
2023-12-29 12:11:11 +0800

    Lab completed

--- JS_1229/guessNumber.js
+++ JS_1229/guessNumber.js
@@ -1,9 +1,10 @@
 // Global variables
-let secretNumber = generateRandomNumber();
+var secretNumber = generateRandomNumber();
 
 // 4. Function to generate a random number between 1 and 10
 function generateRandomNumber() {
-  
+  var ret = Math.floor(Math.random()*10) + 1;
+  return ret;
 }
 
 // Function to check the user's guess
@@ -13,11 +14,22 @@ function checkGuess() {
   let userGuess = document.getElementById("userGuess").value;
   let resultElement = document.getElementById("result");
 
+
+
   // 5. Here should be an if-else statement.
 
-  // If the answer is correct, then output "Congratulations! You guessed the correct number!" with picture2 and music2.
 
+  // If the answer is correct, then output "Congratulations! You guessed the correct number!" with picture2 and music2.
+  if(userGuess == secretNumber){
+    resultElement.innerHTML = "Congratulations! You guessed the correct number!";
+    showImageAndPlaySound('picture2.jpeg', 'music2.mp3');
+    secretNumber = generateRandomNumber();
+  }
   // If the answer is wrong, then output "Sorry, try again!" with picture1 and music1.
+  else{
+    resultElement.innerHTML = "Sorry, try again!";
+    showImageAndPlaySound('picture1.jpeg', 'music1.mp3');
+  }
 
   // P.S. Generate a new random number for the next round after getting the correct answer.
 
@@ -38,4 +50,11 @@ function showImageAndPlaySound(imageSource, soundSource) {
   resultContainer.innerHTML = "";
   resultContainer.appendChild(imageElement);
   resultContainer.appendChild(soundElement);
+}
+
+function handleEnter(event) {
+  if (event.keyCode === 13) { // 13 代表 Enter 鍵的鍵碼
+      event.preventDefault(); // 防止默認的 Enter 鍵行為
+      checkGuess(); // 執行檢查猜測的函數
+  }
 }
